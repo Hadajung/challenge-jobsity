cb0ee375d91f96fb9deddfee3f313cfbbea5cf90
DA JUNG HA <hada@devires.com.br>
2021-03-15 04:52:22 -0300

    Update shows service

--- src/service/shows.service.ts
+++ src/service/shows.service.ts
@@ -1,6 +1,15 @@
-import {ShowDetail} from '../interfaces';
+import {ShowDetail, SearchItem} from '../interfaces';
+import {Episode} from '../interfaces/episode.types';
 import {AXIOSBASE} from './index';
 
-export const fetchShowsWithePage = (page: number = 0) => {
+export const fetchShowsWithPage = (page: number = 0) => {
   return AXIOSBASE.get<ShowDetail[]>(`shows?page=${page}`);
+};
+
+export const searchShows = (text: string) => {
+  return AXIOSBASE.get<SearchItem[]>(`search/shows?q=${text}`);
+};
+
+export const listEpisodes = (id: number) => {
+  return AXIOSBASE.get<Episode[]>(`shows/${id}/episodes`);
 };
