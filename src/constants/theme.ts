b5927b43f2d8fa6ff6a1a34ed18ea8d4f02fabcc
DA JUNG HA <hada@devires.com.br>
2021-03-15 00:32:02 -0300

    Add constans folder content mock and theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,26 @@
+export const Colors = {
+  Black: '#000000',
+  Red: '#FF0101',
+  White: '#ffffff',
+  Gray: '#737373',
+};
+
+export const Fonts = {
+  MulishBold: 'Mulish-Bold',
+  MulishMedium: 'Mulish-Medium',
+  MulishRegular: 'Mulish-Regular',
+  MulishSemiBold: 'Mulish-SemiBold',
+};
+
+export const SystemIcons = {
+  heart: require('../../assets/icons/heart.png'),
+  home: require('../../assets/icons/home.png'),
+  menu: require('../../assets/icons/menu.png'),
+  search: require('../../assets/icons/search.png'),
+  chevronDown: require('../../assets/icons/chevron-down.png'),
+  chevronUp: require('../../assets/icons/chevron-up.png'),
+};
+
+export const SystemImages = {
+  preview: require('../../assets/images/preview.png'),
+};
