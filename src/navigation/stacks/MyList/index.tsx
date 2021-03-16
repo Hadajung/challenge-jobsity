4be7a2055894f5b18a61855c2eb62db0e95c1e60
DA JUNG HA <hada@devires.com.br>
2021-03-16 01:51:42 -0300

    Update my list stack

--- src/navigation/stacks/MyList/index.tsx
+++ src/navigation/stacks/MyList/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {MyListScreen, ShowDetailScreen} from '../../../screens';
+import {StackConfig} from '../../config';
 
 const MyListStack = createStackNavigator();
 
 const MyListStackScreen = (StackProps: any) => {
   return (
-    <MyListStack.Navigator screenOptions={{headerShown: false}}>
-      <MyListStack.Screen name="MyList">
+    <MyListStack.Navigator screenOptions={StackConfig}>
+      <MyListStack.Screen name="MyList" options={{headerShown: false}}>
         {(props: any) => <MyListScreen {...{...props, ...StackProps}} />}
       </MyListStack.Screen>
       <MyListStack.Screen name="ShowDetailScreen">
